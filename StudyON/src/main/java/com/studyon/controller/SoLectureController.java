31cd1cd1d08b3cbb04fbf1acfdce9780e675a0ce
HoMill <HoMill9407@gmail.com>
2020-10-28 16:35:07 +0900

    Ajax로 파일 업로드

--- StudyON/src/main/java/com/studyon/controller/SoLectureController.java
+++ StudyON/src/main/java/com/studyon/controller/SoLectureController.java
@@ -1,24 +1,89 @@
 package com.studyon.controller;
 
-import javax.servlet.http.HttpSession;
+import java.io.File;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
 
 @Controller
 @AllArgsConstructor
 @RequestMapping("SoLecture")
 public class SoLectureController {
 	
-	private  static final Logger log = LoggerFactory.getLogger(SoLectureController.class);
+	private static final Logger log = LoggerFactory.getLogger(SoLectureController.class);
+	
+	// 파일 저장 위치
+//	private static final String uploadFolder = "/Volumes/HoMill D drive/Programing_Project/StudyON_Upload/lecture"; 
+	
 	
 	@GetMapping("So_Lectinsert")
 	public void So_lectinsert() {
 		log.info("Controller : lectinsert....");
+	}
+	
+	
+	// MultipartFile 타입
+	
+//	@PostMapping("/uploadFormAction")
+//	public void uploadFormPost(MultipartFile[] uploadFile, Model model) {
+//		
+//		for (MultipartFile multipartFile : uploadFile) {
+//			
+//			log.info("----------------------------------------------------");
+//			log.info("Upload File Name : " + multipartFile.getOriginalFilename());
+//			log.info("Upload File Size : " + multipartFile.getSize());
+//			
+//			File saveFile = new File(uploadFolder, multipartFile.getOriginalFilename());
+//			
+//			try {
+//				multipartFile.transferTo(saveFile);
+//			}catch(Exception e) {
+//				log.error(e.getMessage());
+//			} // end of catch
+//			
+//		} // end of for
+//	
+//	}
+	
+	// Ajax 타입
+	
+	@PostMapping("/uploadAjaxAction")
+	public void uploadAjaxPost(MultipartFile[] uploadFile) {
+		
+		log.info("update ajax post.........");
+		
+		String uploadFolder = "/Volumes/HoMill D drive/Programing_Project/StudyON_Upload/lecture";
+		
+		for (MultipartFile multipartFile : uploadFile) {
+			
+			log.info("------------------------------------------------");
+			log.info("Upload File Name : " + multipartFile.getOriginalFilename());
+			log.info("Upload File Size : " + multipartFile.getSize());
+			
+			String uploadFileName = multipartFile.getOriginalFilename();
+			
+			// IE has file path
+			uploadFileName = uploadFileName.substring(uploadFileName.lastIndexOf("\\") + 1);
+			log.info("only file name : " + uploadFileName);
+			
+			File saveFile = new File(uploadFolder, uploadFileName);
+			
+			try {
+				
+				multipartFile.transferTo(saveFile);
+			}catch(Exception e) {
+				log.error(e.getMessage());
+			} // end of catch
+			 
+		} // end of for
 	}
 }
---
+++ StudyON/src/main/java/com/studyon/controller/UploadController.java
@@ -0,0 +1,21 @@
+package com.studyon.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.AllArgsConstructor;
+
+@Controller
+@AllArgsConstructor
+public class UploadController {
+	
+	private static final Logger log = LoggerFactory.getLogger(UploadController.class);
+	
+	@GetMapping("/uploadForm")
+	public void uploadForm() {
+		
+		log.info("upload form");
+	}
+}
